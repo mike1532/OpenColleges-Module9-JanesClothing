96428583c87a702c55f92b333fec5bccc07b1798
Michael Ormond <michael.ormond@hotmail.com>
2018-10-15 21:52:34 +1000

    Added listview to CustomerView design, and set up to display Customers table info. Started to write DIsplayCustomers method
    DISREGARD THE DESCRIPTION IN GITIGNORE FILE =D

--- JanesClothingDB/Presentation Layer/CustomersView.Designer.cs
+++ JanesClothingDB/Presentation Layer/CustomersView.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace JanesClothingDB.Presentation_Layer
 {
-    partial class CustomersView
+    partial class frmCustomersView
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lvCustomers = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvCustomers
+            // 
+            this.lvCustomers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.lvCustomers.FullRowSelect = true;
+            this.lvCustomers.GridLines = true;
+            this.lvCustomers.Location = new System.Drawing.Point(13, 13);
+            this.lvCustomers.Name = "lvCustomers";
+            this.lvCustomers.Size = new System.Drawing.Size(957, 383);
+            this.lvCustomers.TabIndex = 0;
+            this.lvCustomers.UseCompatibleStateImageBehavior = false;
+            this.lvCustomers.View = System.Windows.Forms.View.Details;
+            this.lvCustomers.SelectedIndexChanged += new System.EventHandler(this.lvCustomers_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(895, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Close";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Customer ID";
+            this.columnHeader1.Width = 85;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 85;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Name";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 85;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Address";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 85;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Suburb";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 85;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Postcode";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 85;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Gender";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader7.Width = 85;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Category";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader8.Width = 85;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Send Catalogue";
+            this.columnHeader9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader9.Width = 120;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "State";
+            this.columnHeader10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader10.Width = 85;
+            // 
+            // frmCustomersView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CustomersView";
+            this.ClientSize = new System.Drawing.Size(982, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lvCustomers);
+            this.Name = "frmCustomersView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "View Customer Details";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvCustomers;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
     }
 }
--- JanesClothingDB/Presentation Layer/CustomersView.cs
+++ JanesClothingDB/Presentation Layer/CustomersView.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//added to access database and folders
+using System.Data.SqlClient;
+using JanesClothingDB.Data_Access_Layer;
+using JanesClothingDB.Business_Logic_Layer;
 
 namespace JanesClothingDB.Presentation_Layer
 {
-    public partial class CustomersView : Form
+    public partial class frmCustomersView : Form
     {
-        public CustomersView()
+        public frmCustomersView()
         {
             InitializeComponent();
+        }
+
+        private void lvCustomers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //when called, will populate the ListView control with rows from the Customers table
+        public void DisplayCustomers()
+        {
+            string selectQuery = " SELECT Customers.CustomersID, Categories.Category, Customers.FirstName, Customers.LastName, "
+                + " Customers.Address, Customers.Suburb, Customers.State, Customers.Postcode, "
+                + " Customers.Gender, Customers.SendCatalogue FROM Customers INNER JOIN "
+                + "Categories ON Customers.CustomerID = Categories.CategoryID";
+
+            //calls DatabaseConnection method from ConnectionManager.cs file and instantiate DataReaderObject
+            SqlConnection conn = ConnectionManager.DatabaseConnection();            
+            SqlDataReader rdr = null;
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(selectQuery, conn);
+                //TO-DO finish method code
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unsuccessfull" + ex);
+            }
         }
     }
 }
